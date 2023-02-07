5b15eb7c7e6624123de5d53270e70bae1a44cd7e
Jimmy <jimmyzhang1@hotmail.com>
2023-02-06 18:47:02 -0800

    Add discussion list and button components

---
+++ pawhub-frontend/src/components/DiscussionListItem.jsx
@@ -0,0 +1,11 @@
+import Buttons from "./Buttons";
+
+export default function DiscussionListItem() {
+  return (
+    <>
+      <Buttons variant="outlined">Swap</Buttons>
+      <Buttons variant="outlined">Meetup</Buttons>
+      <Buttons variant="outlined">Other</Buttons>
+    </>
+  );
+}
