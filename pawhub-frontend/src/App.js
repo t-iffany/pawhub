2dd962ec082c0ab2674971a8a70c5a0d5db8708b
Nikko Bansil <bjohnnikko@gmail.com>
2023-02-07 19:20:30 -0800

    pull changes from remote master to local master

--- pawhub-frontend/src/App.js
+++ pawhub-frontend/src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
 import DiscussionList from './components/Discussion/DiscussionList';
 import NavigationBar from './components/NavigationBar';
-<<<<<<< HEAD
 import Map from './components/Map';
-=======
 import Login from './components/Login';
->>>>>>> c08991c (Add login route, create login_controller, add basic auth to Login.jsx)
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
