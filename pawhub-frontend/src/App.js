90be8b5cf021f5532fd0a634250f2a496ba37410
t-iffany <tiffanyxleong@gmail.com>
2023-02-07 18:10:34 -0800

    Add login route, create login_controller, add basic auth to Login.jsx

--- pawhub-frontend/src/App.js
+++ pawhub-frontend/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import DiscussionList from './components/Discussion/DiscussionList';
 import NavigationBar from './components/NavigationBar';
+<<<<<<< HEAD
 import Map from './components/Map';
+=======
+import Login from './components/Login';
+>>>>>>> c08991c (Add login route, create login_controller, add basic auth to Login.jsx)
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-
 function App() {
+
   return (
     <Router>
       <div className="App">
@@ -15,7 +19,7 @@ function App() {
           <Routes>
             <Route path="/map" element={<Map />} />
             <Route path="/discussions" element={<DiscussionList />} />
-
+            <Route path="/login" element={<Login />} /> 
           </Routes>
         </div>
       </div>
--- pawhub-rails/config/routes.rb
+++ pawhub-rails/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     resources :discussions
     resources :users
     resources :comments
+    resources :login
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
