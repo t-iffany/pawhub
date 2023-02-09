1c1f460f32e7af1830080b379db72c60607c0bc6
Nikko Bansil <bjohnnikko@gmail.com>
2023-02-09 15:20:34 -0800

    add a component for comment list

---
+++ pawhub-frontend/src/components/Discussion/CommentList.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import moment from "moment";
+
+export default function CommentList ({ comments }) {
+  return (
+    <div className="comments-list-container">
+      <h2 className="comments-list-title">Comments</h2>
+      <div className="comments-list-scrollable">
+        {comments.map((comment, index) => (
+          <div key={index} className="comment">
+            <div className="comment-header">
+              <h3 className="comment-author">Commenter's name</h3>
+              <span className="comment-date">{moment(comment.created_at).fromNow()}</span>
+            </div>
+            <p className="comment-text">{comment.body}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
