e7899ca458900704d3cc5f23f83d526bd4a2ae46
t-iffany <tiffanyxleong@gmail.com>
2023-02-08 18:49:09 -0800

    Implement Edit profile feature

--- pawhub-frontend/src/components/Profile.jsx
+++ pawhub-frontend/src/components/Profile.jsx
@@ -6,6 +6,7 @@ import axios from 'axios';
 export default function Profile() {
 
   const [state, setState] = useState({ users: [] });
+  const [editMode, setEditMode] = useState(false);
 
   useEffect(() => {
     axios
@@ -20,7 +21,7 @@ export default function Profile() {
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  }, [editMode]);
 
   const findUserById = (userId) => {
     if (!state.users) {
@@ -32,6 +33,30 @@ export default function Profile() {
 
   const user = findUserById(3);
 
+  // create an edit form and button that toggles the display of the form
+  const handleEdit = () => {
+    setEditMode(!editMode);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // send PUT request to the server to update the user information
+    axios.put(`http://localhost:3001/api/users/${user.id}`, {
+      username: event.target.username.value,
+      dog_name: event.target.dog_name.value,
+      breed: event.target.breed.value,
+      description: event.target.description.value
+    })
+      .then(res => {
+        // update user information in the state
+        setState({ users: [...state.users, res.data] });
+        setEditMode(false);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   return (
     <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", height: "100vh" }}>
       <div style={{ width: "25%", height: "50%", backgroundColor: "transparent", display: "flex", flexDirection: "column", alignItems: "center" }}>
@@ -40,15 +65,27 @@ export default function Profile() {
           src={user ? user.avatar : "user.avatar not found"}
           sx={{ width: 130, height: 130 }}
         />
+        <button onClick={handleEdit}>Edit</button>
       </div>
       <div style={{ width: "50%", height: "50%", backgroundColor: "transparent" }}>
-        Profile
-        <ul>
-          <li>Name: {user ? user.dog_name : "user.dog_name not found"}</li>
-          <li>Breed: {user ? user.breed : "user.breed not found"}</li>
-          <li>Description: {user ? user.description : "user.description not found"}</li>
-          <li>Social Media Links</li>
-        </ul>
+        {editMode ?
+          <form onSubmit={handleSubmit}>
+            <input type="text" name="username" defaultValue={user ? user.username : "username"} />
+            <input type="text" name="dog_name" defaultValue={user ? user.dog_name : "dog_name"} />
+            <input type="text" name="breed" defaultValue={user ? user.breed : "breed"} />
+            <textarea type="text" name="description" defaultValue={user ? user.description : "description/content"} />
+            <button type="submit">Save</button>
+          </form>
+          :
+          <div>
+            Profile
+            <ul>
+              <li>Name: {user ? user.dog_name : "user.dog_name not found"}</li>
+              <li>Breed: {user ? user.breed : "user.breed not found"}</li>
+              <li>Description: {user ? user.description : "user.description not found"}</li>
+            </ul>
+          </div>
+        }
       </div>
     </div>
   );
