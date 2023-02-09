a0118150fb912e67cc4a7d8fd5cfde211a796100
Nikko Bansil <bjohnnikko@gmail.com>
2023-02-09 15:20:59 -0800

    add comment seeds and imported commentList

--- pawhub-frontend/src/components/Discussion/DiscussionDetails.jsx
+++ pawhub-frontend/src/components/Discussion/DiscussionDetails.jsx
@@ -2,12 +2,14 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import moment from "moment";
+import CommentList from "./CommentList";
 
 export default function DiscussionDetails() {
   const { id } = useParams();
   const [state, setState] = useState({
     discussion: {},
-    user: {}
+    user: {},
+    comments: []
   });
 
   useEffect(() => {
@@ -24,16 +26,25 @@ export default function DiscussionDetails() {
           ...prev,
           user: res.data
         }));
+        return axios.get('http://localhost:3001/api/comments');
+      })
+      .then((res)=> {
+        setState((prev) => ({
+          ...prev,
+          comments: res.data
+        }));
       })
       .catch((err) => console.log(err));
   }, []);
 
   return (
     <div className="blog-post">
-    <h2 className="title">{state.discussion.title}</h2>
-    <p className="author">by {state.user.username}</p>
-    <p className="content">{state.discussion.content}</p>
-    <p className="date">posted {moment(state.discussion.created_at).fromNow()} </p>
-  </div>
+      <h2 className="title">{state.discussion.title}</h2>
+      <p className="author">by {state.user.username}</p>
+      <p className="content">{state.discussion.content}</p>
+      <p className="date">posted {moment(state.discussion.created_at).fromNow()} </p>
+      <CommentList comments={state.comments}/>
+    </div>
+    
   );
 }
--- pawhub-rails/db/seeds.rb
+++ pawhub-rails/db/seeds.rb
@@ -69,8 +69,18 @@ user3.discussions.create({
 # Comments
 Comment.destroy_all
 
-comment1 = Comment.create({body: 'sick post!', user_id: 1, discussion_id: 1})
-
-
-
-
+comment1 = Comment.create({body: 'Vel pharetra vel turpis nunc eget lorem. In arcu cursus euismod quis. Arcu felis bibendum ut tristique et egestas quis ipsum suspendisse. Mus mauris vitae ultricies leo integer malesuada nunc vel risus. Pharetra vel turpis nunc eget. Mi ipsum faucibus vitae aliquet.', user_id: 3, discussion_id: 1})
+Comment.create({body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus aenean vel elit scelerisque. Bibendum at varius vel pharetra vel turpis nunc eget lorem. Sagittis vitae et leo duis ut diam quam nulla. Id cursus metus aliquam eleifend. Amet justo donec enim diam vulputate ut pharetra sit amet. Sit amet nulla facilisi morbi tempus iaculis', user_id: 2, discussion_id: 3})
+Comment.create({body: 'Vulputate mi sit amet mauris commodo quis. Faucibus et molestie ac feugiat sed lectus vestibulum mattis. Aliquet bibendum enim facilisis gravida neque convallis a cras. Et egestas quis ipsum suspendisse ultrices gravida. Egestas sed tempus urna et pharetra pharetra.', user_id: 1, discussion_id: 1})
+Comment.create({body: 'Aliquet nec ullamcorper sit amet. Sed egestas egestas fringilla phasellus faucibus scelerisque eleifend donec. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus.', user_id: 1, discussion_id: 1})
+Comment.create({body: 'Mi ipsum faucibus vitae aliquet. Maecenas volutpat blandit aliquam etiam erat. Nibh sit amet commodo nulla facilisi. Nibh nisl condimentum id venenatis a.', user_id: 2, discussion_id: 2})
+Comment.create({body: 'Risus viverra adipiscing at in tellus integer feugiat scelerisque varius. Magna ac placerat vestibulum lectus mauris ultrices eros in cursus. Morbi tristique senectus et netus et malesuada fames ac turpis.', user_id: 1, discussion_id: 3})
+Comment.create({body: 'Pretium vulputate sapien nec sagittis aliquam malesuada. Eget egestas purus viverra accumsan in nisl.', user_id: 3, discussion_id: 1})
+Comment.create({body: 'Nibh praesent tristique magna sit amet purus. Tempor commodo ullamcorper a lacus vestibulum sed. At varius vel pharetra vel. Turpis egestas maecenas pharetra convallis posuere morbi leo urna. Sit amet nisl purus in mollis nunc.', user_id: 3, discussion_id: 2})
+Comment.create({body: 'Pulvinar mattis nunc sed blandit libero volutpat sed cras ornare. Nunc lobortis mattis aliquam faucibus purus in massa tempor. ', user_id: 2, discussion_id: 2})
+Comment.create({body: 'Nisi est sit amet facilisis magna etiam. Arcu felis bibendum ut tristique et egestas quis ipsum. Netus et malesuada fames ac turpis egestas.', user_id: 3, discussion_id: 2})
+Comment.create({body: 'A condimentum vitae sapien pellentesque. Lacinia at quis risus sed vulputate odio ut enim. Sodales ut eu sem integer vitae. Neque gravida in fermentum et sollicitudin ac orci. Etiam non quam lacus suspendisse faucibus interdum.', user_id: 3, discussion_id: 3})
+Comment.create({body: 'Velit euismod in pellentesque massa placerat duis ultricies. A arcu cursus vitae congue mauris rhoncus aenean vel elit. Id donec ultrices tincidunt arcu. Gravida in fermentum et sollicitudin ac.', user_id: 3, discussion_id: 1})
+Comment.create({body: 'Vitae ultricies leo integer malesuada nunc vel risus commodo viverra. Malesuada bibendum arcu vitae elementum curabitur vitae nunc sed. Phasellus vestibulum lorem sed risus ultricies.', user_id: 1, discussion_id: 3})
+Comment.create({body: 'Faucibus purus in massa tempor nec feugiat nisl pretium fusce. Orci porta non pulvinar neque laoreet suspendisse.', user_id: 2, discussion_id: 2})
+Comment.create({body: 'Fermentum odio eu feugiat pretium. Quisque non tellus orci ac auctor augue mauris. Morbi tristique senectus et netus et malesuada. Feugiat sed lectus vestibulum mattis ullamcorper.', user_id: 1, discussion_id: 1})
