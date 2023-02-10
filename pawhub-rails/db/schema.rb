d1e01afb2ce143da53949a97348ebdb8dc97392f
t-iffany <tiffanyxleong@gmail.com>
2023-02-10 13:24:50 -0800

    Remove old image column from users table

--- pawhub-rails/db/schema.rb
+++ pawhub-rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_10_044022) do
+ActiveRecord::Schema.define(version: 2023_02_10_044620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,7 +49,6 @@ ActiveRecord::Schema.define(version: 2023_02_10_044022) do
     t.string "dog_name"
     t.string "breed"
     t.text "description"
-    t.string "image"
     t.string "avatar"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
