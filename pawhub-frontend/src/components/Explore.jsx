027d4b2b0d2f921138158cdb6decbef9e050c071
Jimmy <jimmyzhang1@hotmail.com>
2023-02-11 14:58:44 -0800

    Add images from database into carousel. Add user info on each image.

--- pawhub-frontend/src/components/Explore.jsx
+++ pawhub-frontend/src/components/Explore.jsx
@@ -1,21 +1,52 @@
 import * as React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 import { Carousel, CarouselItem } from "react-round-carousel";
 import "react-round-carousel/src/index.css";
 
 export default function Explore() {
-  const items = Array(20)
+  const [state, setState] = useState({ users: [], images: [] });
+
+  useEffect(() => {
+    Promise.all([
+      axios.get("http://localhost:3001/api/users"),
+      axios.get("http://localhost:3001/api/images"),
+    ])
+      // Our res is an array of the response received: [{users}, {images}]
+      .then((response) => {
+        setState((prev) => ({
+          ...prev,
+          users: response[0].data,
+          images: response[1].data,
+        }));
+      })
+
+      .catch((err) => console.log(err));
+  }, []);
+
+  console.log("State.images", state.images);
+
+  const getUserByImage = (index) => {
+    return state.users.find((user) => user.id === state.images[index].user_id);
+  };
+
+  const items = Array(state.images.length)
     .fill("")
-    .map((string, index) => ({
-      alt: "A random photo",
-      image: `https://picsum.photos/${210 + index}`,
+    .map((image, index) => ({
+      alt: `${getUserByImage(index).username}`,
+      image: `data:image/jpeg;base64,${state.images[index].file_data}`,
+      height: "190",
+      width: "190",
       content: (
-        <div onClick={() => console.log("clicked")}>
-          <strong>Username</strong>
-          <span>Dog Name</span>
+        <div key={index} onClick={() => console.log("clicked")}>
+          <strong>{getUserByImage(index).username}</strong>
+          <span>{getUserByImage(index).dog_name}</span>
         </div>
       ),
     }));
+
+  console.log("items", items);
 
   return (
     <div>
