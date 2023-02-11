111b1a747f6f4e70375a8b21ccc30b658bcc0078
t-iffany <tiffanyxleong@gmail.com>
2023-02-10 22:17:28 -0800

    Update profile.jsx to dynamically render data according to logged in user and not hardcoded user

--- pawhub-frontend/src/components/Profile.jsx
+++ pawhub-frontend/src/components/Profile.jsx
@@ -2,7 +2,7 @@ import Avatar from '@mui/material/Avatar';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-export default function Profile() {
+export default function Profile({currentUser}) {
 
   const [state, setState] = useState({ users: [], images: [] });
   const [editMode, setEditMode] = useState(false);
@@ -20,7 +20,6 @@ export default function Profile() {
         setState((prev) => ({
           ...prev,
           users: response[0].data,
-          // images: `data:image/jpeg;base64,${response[1].data.file_data}`
           images: response[1].data
         }));
       })
@@ -37,7 +36,7 @@ export default function Profile() {
     return state.users.find((user) => user.id === userId);
   };
 
-  const user = findUserById(3);
+  //const user = findUserById(3);
 
   // create an edit form and button that toggles the display of the form
   const handleEdit = () => {
@@ -48,7 +47,7 @@ export default function Profile() {
     event.preventDefault();
     setUploading(true);
     // send PUT request to the server to update the user information
-    axios.put(`http://localhost:3001/api/users/${user.id}`, {
+    axios.put(`http://localhost:3001/api/users/${currentUser.id}`, {
       username: event.target.username.value,
       dog_name: event.target.dog_name.value,
       breed: event.target.breed.value,
@@ -60,7 +59,7 @@ export default function Profile() {
 
         const imageData = new FormData();
         imageData.append("file_data", selectedFile);
-        imageData.append("user_id", user.id);
+        imageData.append("user_id", currentUser.id);
 
         axios
           .post("http://localhost:3001/api/images", imageData, {
@@ -94,9 +93,9 @@ export default function Profile() {
   return (
     <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", height: "100vh" }}>
       <div style={{ width: "25%", height: "50%", backgroundColor: "transparent", display: "flex", flexDirection: "column", alignItems: "center" }}>
-        <div>{user ? user.username : "user not found"}</div>
+        <div>{currentUser ? currentUser.username : "user not found"}</div>
         <Avatar alt='avatar'
-          src={user ? user.avatar : "user.avatar not found"}
+          src={currentUser ? currentUser.avatar : "user.avatar not found"}
           sx={{ width: 130, height: 130 }}
         />
         <button onClick={handleEdit}>Edit</button>
@@ -104,10 +103,10 @@ export default function Profile() {
       <div style={{ width: "50%", height: "50%", backgroundColor: "transparent" }}>
         {editMode ?
           <form onSubmit={handleSubmit}>
-            <input type="text" name="username" defaultValue={user ? user.username : "username"} />
-            <input type="text" name="dog_name" defaultValue={user ? user.dog_name : "dog_name"} />
-            <input type="text" name="breed" defaultValue={user ? user.breed : "breed"} />
-            <textarea type="text" name="description" defaultValue={user ? user.description : "description/content"} />
+            <input type="text" name="username" defaultValue={currentUser ? currentUser.username : "username"} />
+            <input type="text" name="dog_name" defaultValue={currentUser ? currentUser.dog_name : "dog_name"} />
+            <input type="text" name="breed" defaultValue={currentUser ? currentUser.breed : "breed"} />
+            <textarea type="text" name="description" defaultValue={currentUser ? currentUser.description : "description/content"} />
             <input 
               type="file" 
               name="image" 
@@ -122,9 +121,9 @@ export default function Profile() {
           <div>
             Profile
             <ul>
-              <li>Name: {user ? user.dog_name : "user.dog_name not found"}</li>
-              <li>Breed: {user ? user.breed : "user.breed not found"}</li>
-              <li>Description: {user ? user.description : "user.description not found"}</li>
+              <li>Name: {currentUser ? currentUser.dog_name : "user.dog_name not found"}</li>
+              <li>Breed: {currentUser ? currentUser.breed : "user.breed not found"}</li>
+              <li>Description: {currentUser ? currentUser.description : "user.description not found"}</li>
             </ul>
             <div>
               { state.images && 
@@ -132,7 +131,7 @@ export default function Profile() {
                   <img 
                     key={index}
                     src={`data:image/jpeg;base64,${image.file_data}`} 
-                    alt="dog" 
+                    alt={currentUser.dog_name} 
                     width="150" 
                     height="150" />  
                 ))
