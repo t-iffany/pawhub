58d1b4730dd07f42a705abe6205ef61bb58e0dfe
Jimmy <jimmyzhang1@hotmail.com>
2023-02-12 22:28:41 -0800

    Add search bar that filters through dog breeds

--- pawhub-frontend/src/components/Resources/Breeds.jsx
+++ pawhub-frontend/src/components/Resources/Breeds.jsx
@@ -2,9 +2,12 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { ListGroup } from "react-bootstrap";
 import BreedsItem from "./BreedsItem";
+import { TextField } from "@mui/material";
 
 export default function Breeds() {
   const [breedList, setBreedList] = useState([]);
+  const [query, setQuery] = useState(null);
+
   const options = {
     method: "GET",
     url: "https://dog-breeds2.p.rapidapi.com/dog_breeds",
@@ -18,30 +21,66 @@ export default function Breeds() {
     window.open(url, "_blank", "noreferrer");
   };
 
+  function filterByQuery(breedsList, query) {
+    return breedsList.filter((breed) =>
+      breed.breed.toLowerCase().includes(query.toLowerCase())
+    );
+  }
+
   console.log(breedList);
+  console.log(query);
 
   useEffect(() => {
     axios
       .request(options)
-      .then((res) => setBreedList(res.data))
+      .then((res) => {
+        console.log(res.data);
+        setBreedList(res.data);
+      })
       .catch((err) => console.log(err));
   }, []);
 
   return (
     <div>
       <h1 className="page-header">Breeds</h1>
+
+      <TextField
+        id="outlined-basic"
+        label="Search for any dog!"
+        variant="outlined"
+        className="breed-search"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+
       <ListGroup>
-        {breedList.map((breed) => {
-          return (
-            <BreedsItem
-              key={breed.id}
-              breed={breed.breed}
-              origin={breed.origin}
-              img={breed.img}
-              onClick={() => openInNewTab(breed.url)}
-            />
-          );
-        })}
+        {breedList &&
+          !query &&
+          breedList.map((breed) => {
+            return (
+              <BreedsItem
+                key={breed.id}
+                breed={breed.breed}
+                origin={breed.origin}
+                img={breed.img}
+                onClick={() => openInNewTab(breed.url)}
+              />
+            );
+          })}
+
+        {breedList &&
+          query &&
+          filterByQuery(breedList, query).map((breed) => {
+            return (
+              <BreedsItem
+                key={breed.id}
+                breed={breed.breed}
+                origin={breed.origin}
+                img={breed.img}
+                onClick={() => openInNewTab(breed.url)}
+              />
+            );
+          })}
       </ListGroup>
     </div>
   );
