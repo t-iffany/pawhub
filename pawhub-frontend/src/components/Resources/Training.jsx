eac0fc5fc9f4b5d25dec772851a814461fe11087
t-iffany <tiffanyxleong@gmail.com>
2023-02-13 01:37:23 -0800

    Add api_keys file, import into Training.jsx, and include in .gitignore

--- pawhub-frontend/src/components/Resources/Training.jsx
+++ pawhub-frontend/src/components/Resources/Training.jsx
@@ -1,15 +1,22 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import API_KEY from '../api_keys';
+
 
 export default function TrainingVideos() {
   const [videos, setVideos] = useState([]);
   const [videoTitles, setVIdeoTitles] = useState([]);
 
+  //console.log("api key: ", API_KEY)
+
   // useEffect makes API request and retrieve video IDs when the component is mounted
   // the component then renders the 'Video' component for each video ID, passing the video ID as a prop
   useEffect(() => {
     const fetchData = () => {
-      const API_KEY = '';
+      const obj = API_KEY;
+      const YOUTUBE_API_KEY = obj.key;
+      //console.log("KEY: ", KEY)
+
       // search.list allows you to search for videos that match a specified query term
       // 'part' paramenter specifies the query term you want to search, typically for videos set it to 'id'
       // 'q' parameter specifies the query term you want to search for
@@ -21,7 +28,7 @@ export default function TrainingVideos() {
           `?part=id` +
           `&q=dog+training` +
           `&type=video` +
-          `&key=${API_KEY}`
+          `&key=${YOUTUBE_API_KEY}`
         )
         .then(response => {
           setVideos(response.data.items);
@@ -35,16 +42,18 @@ export default function TrainingVideos() {
     fetchData();
   }, []);
 
+
   return (
     // use Youtube iframe player API to embed the videos
     <div>
-      {videos.map(video => (
+      {videos.map((video, index) => (
         <iframe
           key={video.id.videoId}
           src={`https://www.youtube.com/embed/${video.id.videoId}`}
           frameBorder="0"
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in- picture"
           allowFullScreen
+          title={videoTitles[index]}
         />
       ))}
     </div>
